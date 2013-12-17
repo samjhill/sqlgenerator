3e6a1d74120e957b84391efa4fe24f7371e1e3bc
Sam Hill <samuhill@gmail.com>
2013-12-17 12:53:11 -0500

    fix more broken links

--- help/assets/php/links.php
+++ help/assets/php/links.php
@@ -1,7 +1,7 @@
 <?php
     echo '<link rel="shortcut icon" href="images/favicon.ico" />
-		<link type="text/css" rel="stylesheet" href="libraries/font-awesome/css/font-awesome.css" />
-		<link type="text/css" rel="stylesheet" href="libraries/bootstrap/css/bootstrap.min.css" />
-		<link type="text/css" rel="stylesheet" href="css/style.css"/>
+		<link type="text/css" rel="stylesheet" href="assets/libraries/font-awesome/css/font-awesome.css" />
+		<link type="text/css" rel="stylesheet" href="assets/libraries/bootstrap/css/bootstrap.min.css" />
+		<link type="text/css" rel="stylesheet" href="assets/css/style.css"/>
 		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>';
 ?>
