a3142bea0576c1eb393aa40fc89a16f08aab1986
Sam Hill <samuhill@gmail.com>
2013-12-17 11:43:35 -0500

    Add shell for help docs

---
+++ help/index.php
@@ -0,0 +1,28 @@
+<?php
+  $currentPage="Home";
+?>
+
+<!DOCTYPE html>
+<html lang='en'>
+    <head>
+		<meta charset='utf-8' />
+		<title>Home - Help for SQLGenerator</title>
+		<?php include("php/links.php"); ?>
+	</head>
+
+    <body>		
+		<!--php navigation include-->
+		<?php include("php/navigation.php"); ?>
+		
+        <div id="content">
+			
+			<h1>Start your navigation here</h1>
+            
+			<div id="homeNav">
+			</div>	
+			
+        </div>
+		
+	  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+</html>
